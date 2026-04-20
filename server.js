b9db36ecdc5ecd750bbb067bbc92ca506c2947b5
illson-lll <bobrovnik.illiya@lll.kpi.ua>
2026-04-20 12:08:39 +0300

    r1

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+
+
+app.use(express.static(path.join(__dirname, 'web')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web/index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущено на http://localhost:${PORT}`);
+});
