0b7aa9dc274b5658b98a447b87774d461fdee648
illson-lll <bobrovnik.illiya@lll.kpi.ua>
2026-05-09 21:11:57 +0300

    Test auth; Alert toast; error msg;

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const app = express();
 const PORT = 3000;
 
+const users = [];
+
 app.use(express.json());
-
-
-
 
 app.use(express.static(path.join(__dirname, 'web')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'web/index.html'));
+});
+
+
+app.post('/api/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = users.find(u => u.username === username);
+        if (user) {
+            return res.status(400).json({ message: 'Користувач вже існує!' });
+        }
+
+        const hashpass = await bcrypt.hash(password, 10);
+        users.push({ username, password: hashpass }); // Пуш в дб
+        console.log("User created:", username);
+        res.status(201).json({ message: "Користувач створений!" });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Помилка сервера" });
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = users.find(u => u.username === username);
+        if (user == null) {
+            return res.status(400).json({ message: 'Користувача не знайдено' });; // Перевірка існування
+        }
+        else {
+            if (await bcrypt.compare(password, user.password)) { // Перевірка пароля
+                const token = jwt.sign({ name: user.username }, 'SECRET_KEY_123');
+                res.json({ accessToken: token, username: user.username });
+            } else {
+                res.status(400).json({ message: 'Невірний пароль' });;
+            }
+        }
+    } catch {
+        console.error(err);
+        res.status(500).json({ message: "Помилка сервера" });
+    }
 });
 
 
--- web/script.js
+++ web/script.js
@@ -1,22 +1,137 @@
 (() => {
   'use strict'
 
+  // Змінні
+  let isLoggedIn = false;
+
+  // Функції
+
+  // Спливаюче повідомлення
+  function showToast(msg) {
+    const toastLive = document.getElementById('liveToast')
+    const toast_text = document.getElementById("toast-text");
+
+    const toast = bootstrap.Toast.getOrCreateInstance(toastLive)
+    if (toast_text) toast_text.innerText = msg;
+    toast.show()
+  }
+
+  // Помилка при авторизації
+  function showError(elementId, message) {
+    const error_el = document.getElementById(elementId);
+    if (error_el) {
+      error_el.innerText = message;
+
+      error_el.classList.add('show');
+      setTimeout(() => {
+        error_el.classList.remove('show');
+      }, 3000);
+    }
+  }
+  // Реєстрація
+  async function registerUser(username, password) {
+    console.log(`logging ${username}`);
+    try {
+      const response = await fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        showToast("Реєстрація успішна!");
+        const regModal = bootstrap.Modal.getInstance(document.getElementById('reg-modal'));
+        if (regModal) regModal.hide();
+        new bootstrap.Modal(document.getElementById('login-modal')).show();
+      } else {
+        showError('reg-error', data.message || "Помилка реєстрації");
+      }
+    } catch (error) {
+      showError('reg-error', "Сервер не доступний");
+    }
+  }
+  // Вхід
+  async function loginUser(username, password) {
+    console.log(`logging ${username}`);
+    try {
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('username', data.username);
+        const profile_text = document.getElementById("profile-text");
+        if (profile_text) profile_text.innerText = data.username;
+        const loginModal = bootstrap.Modal.getInstance(document.getElementById('login-modal'));
+        if (loginModal) loginModal.hide();
+        const regModal = bootstrap.Modal.getInstance(document.getElementById("reg-modal"));
+        if (regModal) regModal.hide();
+
+        isLoggedIn = true;
+        showToast("Ви ввійшли!")
+      } else {
+        showError('login-error', data.message || "Невірний логін або пароль");
+      }
+    } catch (error) {
+      showError('login-error', "Сервер не доступний");
+    }
+  }
+
+
+
   document.addEventListener('DOMContentLoaded', () => {
 
     const login_modal = document.getElementById('login-modal');
+    const profile_modal = document.getElementById('profile-modal');
     const profile_btn = document.getElementById('profile-btn');
+    const profile_text = document.getElementById('profile-text');
+    const logout_button = document.getElementById("logout-button");
+
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+
+    if (token && username) {
+      isLoggedIn = true;
+      if (profile_text) profile_text.innerText = username;
+    }
 
     if (login_modal && profile_btn) {
       const loginModal = new bootstrap.Modal(login_modal);
+      const profileModal = new bootstrap.Modal(profile_modal);
       profile_btn.addEventListener('click', () => {
-        loginModal.show();
+        const username = localStorage.getItem("username");
+        if (!username) {
+          loginModal.show();
+        } else {
+          document.getElementById("username-profile").innerText = `Користувач: ${username}`
+          const bestScore = localStorage.getItem("highscore") || 0;
+          document.getElementById('personal-best').innerText = `Найкращий результат: ${bestScore}`;
+          profileModal.show();
+        }
       });
     }
+
+    logout_button.addEventListener('click', () => {
+      localStorage.removeItem("username");
+      localStorage.removeItem("token");
+
+      isLoggedIn = false;
+
+      if (profile_text) profile_text.innerText = "Увійти";
+      showToast("Ви вийшли!");
+    });
 
     const forms = document.querySelectorAll('.needs-validation');
     Array.from(forms).forEach(form => {
 
-      form.addEventListener('submit', event => {
+      form.addEventListener('submit', async event => {
         if (form.id === 'reg-form') {
           const password = document.getElementById('reg-password');
           const confirm = document.getElementById('reg-password-confirm');
@@ -31,8 +146,17 @@
           event.preventDefault();
           event.stopPropagation();
         } else {
-          event.preventDefault();
-          alert("Успіх валідації.");
+          console.log("Успіх валідації.");
+          if (form.id === 'reg-form') {
+            const user = document.getElementById('reg-username').value;
+            const pass = document.getElementById('reg-password').value;
+            registerUser(user, pass);
+          }
+          else if (form.id === 'login-form') {
+            const user = document.getElementById('login-username').value;
+            const pass = document.getElementById('login-password').value;
+            loginUser(user, pass);
+          }
         }
 
         form.classList.add('was-validated');
@@ -41,3 +165,4 @@
 
   });
 })();
+
